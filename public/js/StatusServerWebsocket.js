7e5d02d3d45c50bf31103e7b8cf8a07344a53607
root <root@vps644828.ovh.net>
2019-02-04 12:34:05 +0100

    modif

--- public/js/StatusServerWebsocket.js
+++ public/js/StatusServerWebsocket.js
@@ -36,7 +36,7 @@ FermetureConnexion = function() {
 
 
 StartServeur = function (message){
-  window.open("http://www.quizzlux.cinemalux.org/public/PlayAdmin/WebSocket");
+  window.open("http://51.75.125.249/public/PlayAdmin/WebSocket");
   return false;
 }
 
