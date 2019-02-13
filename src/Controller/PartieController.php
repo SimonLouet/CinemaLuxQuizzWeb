1a4ea3bc40f6fcd881d751c47148c7fbf7e0cef6
LouetSimon <simon.louet.98@gmail.com>
2019-02-13 15:25:27 +0100

    Fiche presentateur

--- src/Controller/PartieController.php
+++ src/Controller/PartieController.php
@@ -28,12 +28,14 @@ class PartieController extends AbstractController
 
     // instantiate and use the dompdf class
     $dompdf = new Dompdf();
-    $dompdf->loadHtml('hello world');
+    $dompdf->loadHtml('<p>hello world</p>');
 
     // (Optional) Setup the paper size and orientation
     $dompdf->setPaper('A4', 'landscape');
 
-
+    // Render the HTML as PDF
+    $dompdf->render();
+    
     // Output the generated PDF to Browser
     $dompdf->stream();
 
