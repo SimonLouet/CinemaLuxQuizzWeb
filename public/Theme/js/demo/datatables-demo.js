da026d6cceb335d5d4adb16da1ea6976046829d4
LouetSimon <simon.louet.98@gmail.com>
2019-02-15 15:08:44 +0100

    modif tour par tour

--- public/Theme/js/demo/datatables-demo.js
+++ public/Theme/js/demo/datatables-demo.js
@@ -1,4 +1,8 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  var i = 1;
+  while ($('#dataTable-' +  i) != null || i< 50) {
+    $('#dataTable-' +  i).DataTable();
+    i++;
+  }
 });
